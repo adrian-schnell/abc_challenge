2e003dd68af5938d42f453f226d3455e475fd32f
Adrian <322735+adrian-schnell@users.noreply.github.com>
2023-04-11 00:15:45 +0200

    add feedback for request

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ChallengeDataRequest;
+use App\Http\Service\GoogleApiService;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+	private GoogleApiService $apiService;
+
+	public function __construct()
+	{
+		$this->apiService = new GoogleApiService();
+	}
+
 	public function receiveResults(ChallengeDataRequest $request): string
 	{
-		return $request->getName();
-    }
+		/**
+		 * if data already exist, update it
+		 */
+		if ($this->apiService->dataExists($request)) {
+			$this->apiService->updateData($request);
+			return 'updated data';
+		}
+		// create new dataset
+		$this->apiService->createData($request);
+
+		return 'created new dataset';
+	}
 }
